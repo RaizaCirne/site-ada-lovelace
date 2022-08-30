e0b9bfb6cef0f56cc01fe10aacd28e69cf40362c
RaizaCirne <raizabraz93@gmail.com>
2022-08-30 19:55:41 -0300

    Inserindo funções js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
-// Criando um alert em Javascript 
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
-// Preencher campos nome, e-mail e telefone
-// Se todos os campos forem preenchidos, mostrar uma mensagem de alerta "Prontinho! você receberá as novidades por email." 
-// Se algum dos campos não forem preenchidos, mostrar uma mensagem de alerta "Por favor, preencha os campos nome, email e telefone!"
-
-// Verificar vídeos sobre validar formulário
+function validaFormulario(){
+    if (document.getElementById("nome").value != "" && 
+        document.getElementById ("email").value != "" && 
+        document.getElementById("telefone").value != ""){
+        alert ("Prontinho! você receberá as novidades por email.")
+        }else{
+            
+        alert("Por favor, preencha os campos nome, email e telefone!")
+        }
+}
