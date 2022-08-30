cbc7058037ffd37fd04ea3a5bc2a9262eda7742f
RaizaCirne <raizabraz93@gmail.com>
2022-08-29 22:34:35 -0300

    Verificando sobre validar formulário

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,5 @@
 // Preencher campos nome, e-mail e telefone
 // Se todos os campos forem preenchidos, mostrar uma mensagem de alerta "Prontinho! você receberá as novidades por email." 
 // Se algum dos campos não forem preenchidos, mostrar uma mensagem de alerta "Por favor, preencha os campos nome, email e telefone!"
+
+// Verificar vídeos sobre validar formulário
