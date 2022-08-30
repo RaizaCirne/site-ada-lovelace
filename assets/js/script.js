e9db04c6a23980414e20ad89eac70149df06fc50
RaizaCirne <raizabraz93@gmail.com>
2022-08-29 22:28:50 -0300

    Criando algoritmo para alerta javascript

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Criando um alert em Javascript 
+
+// Preencher campos nome, e-mail e telefone
+// Se todos os campos forem preenchidos, mostrar uma mensagem de alerta "Prontinho! você receberá as novidades por email." 
+// Se algum dos campos não forem preenchidos, mostrar uma mensagem de alerta "Por favor, preencha os campos nome, email e telefone!"
