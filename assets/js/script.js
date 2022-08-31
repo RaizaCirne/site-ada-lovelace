0273798468d2a510c14ca506c76ec3abf818e073
RaizaCirne <raizabraz93@gmail.com>
2022-08-31 17:37:17 -0300

    Consertando erro em alert JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
-document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+document.getElementById("submit").addEventListener("click", validaFormulario)
 
 function validaFormulario(){
     if (document.getElementById("nome").value != "" && 
-        document.getElementById ("email").value != "" && 
-        document.getElementById("telefone").value != ""){
+        document.getElementById ("email").value != "" &&
+        document.getElementById("tel").value != ""){
         alert ("Prontinho! você receberá as novidades por email.")
         }else{
-            
-        alert("Por favor, preencha os campos nome, email e telefone!")
+            alert("Por favor, preencha os campos nome, email e telefone!")
         }
 }
